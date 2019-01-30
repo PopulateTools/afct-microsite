a06a48c83b513fe6c34c68ff045026fd96b18e16
Hugoren Martinako <aumpfbahn@gmail.com>
2019-01-30 11:22:11 +0100

    real data + labels diff

---
+++ _assets/js/charts.js
@@ -0,0 +1,129 @@
+(function () {
+  Chart.defaults.global.tooltips = {
+    enabled: false
+  }
+
+  getJSON("../static_data/datasets.json", (data) => {
+    const jsonData = JSON.parse(data);
+    
+    const canvas = document.querySelectorAll("canvas")
+    canvas.forEach((chart) => {
+      const chartData = jsonData.find(set => set.id === chart.id)
+  
+      if (chartData) {
+        // prepare data
+        const columnNames = chartData.data.map(a => a[0])
+        const data = chartData.data.map(a => a[1])
+        
+        const isGroupLabel = (chart.dataset.labels === undefined)
+        if (isGroupLabel) {
+          const datasetLabel = chartData.id.match(/^[\d]-[\w]+/)[0]
+          const labels = document.querySelector(`[group-label='${datasetLabel}']`)
+    
+          if (!labels.innerHTML) {
+            let markup = ''
+            columnNames.forEach(column => markup += `<div><span>${column}</span></div>`)
+            labels.innerHTML = markup
+          }
+        }
+  
+        const barThickness = 30
+        chart.height = columnNames.length * barThickness + 8
+  
+        // const maxValue = Math.max.apply(Math, data) * 1.05
+        // const inverseData = data.map(e => maxValue - e)
+  
+        // options
+        var opts = {
+          type: 'horizontalBar',
+          data: {
+            labels: columnNames,
+            datasets: [{
+              data: data,
+              backgroundColor: 'rgba(255, 99, 132)'
+            }]
+          },
+          plugins: [ChartDataLabels],
+          options: {
+            responsive: true,
+            maintainAspectRatio: false,
+            legend: {
+              display: false,
+            },
+            scales: {
+              xAxes: [{
+                stacked: true,
+                gridLines: {
+                  drawBorder: false,
+                  drawTicks: false,
+                },
+                ticks: {
+                  display: false,
+                  beginAtZero: true,
+                  precision: 0,
+                  // max: maxValue
+                }
+              }],
+              yAxes: [{
+                barThickness: barThickness,
+                stacked: true,
+                gridLines: {
+                  display: false
+                },
+                ticks: {
+                  display: false
+                }
+              }]
+            },
+            plugins: {
+              datalabels: {
+                color: '#fff',
+                anchor: 'end',
+                align: 'start'
+              }
+            }
+          }
+        };
+
+        if (!isGroupLabel) {
+          opts = {
+            ...opts,
+            options: {
+              ...opts.options,
+              scales: {
+                ...opts.options.scales,
+                yAxes: [{
+                  barThickness: barThickness,
+                  stacked: true,
+                  gridLines: {
+                    display: false
+                  },
+                  ticks: {
+                    display: true
+                  }
+                }]
+              }
+            }
+          }          
+        }
+  
+        new Chart(chart, opts)
+      }
+    })
+  })
+
+  // Helpers
+  function getJSON(url, callback) {
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', url, true);
+    xobj.onreadystatechange = function () {
+      if (xobj.readyState == 4 && xobj.status == "200") {
+        // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+        callback(xobj.responseText);
+      }
+    };
+    xobj.send(null);
+  }
+
+})();
