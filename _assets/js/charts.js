57af157261ce6cac870ad461efa1f0d31c1a5815
Hugoren Martinako <aumpfbahn@gmail.com>
2019-01-31 17:43:37 +0100

    labels bigger

--- _assets/js/charts.js
+++ _assets/js/charts.js
@@ -4,7 +4,7 @@
   }
 
   Chart.defaults.global.defaultFontFamily = 'Avenir Next';
-  Chart.defaults.global.defaultFontSize = 12;
+  Chart.defaults.global.defaultFontSize = 13;
 
   var elem = document.getElementById("hidden-chart-colors");
   var objColors = {}
