eb9814924a6370361885b811c884a5c544523200
Hugoren Martinako <aumpfbahn@gmail.com>
2020-09-09 17:31:46 +0200

    Update popup-handler.js

--- _assets/js/popup-handler.js
+++ _assets/js/popup-handler.js
@@ -23,6 +23,26 @@
     };
   })();
 
+  function getCookie(name) {
+    // Split cookie string and get all individual name=value pairs in an array
+    var cookieArr = document.cookie.split(";");
+
+    // Loop through the array elements
+    for (var i = 0; i < cookieArr.length; i++) {
+      var cookiePair = cookieArr[i].split("=");
+
+      /* Removing whitespace at the beginning of the cookie name
+        and compare it with the given string */
+      if (name == cookiePair[0].trim()) {
+        // Decode the cookie value and return
+        return decodeURIComponent(cookiePair[1]);
+      }
+    }
+
+    // Return null if not found
+    return null;
+  }
+
   let IS_WIDGET_INITIALIZED = false
   let REPORT_URL = null
 
@@ -47,7 +67,7 @@
     if (triggerModal !== undefined) {
       const widget = document.querySelector("[widgetid^='PopupSignupForm']");
 
-      if (widget) {
+      if (widget && getCookie("MCPopupClosed") !== "yes") {
         e.preventDefault()
         REPORT_URL = href
         widget.style.opacity = null;
